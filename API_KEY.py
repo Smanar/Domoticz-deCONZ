554baccade129f41a88e8cd51db51039eb6d382c
Smanar <aurelien.pra@laposte.net>
2019-01-06 15:43:07 +0100

    Update API_KEY.py

--- API_KEY.py
+++ API_KEY.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 # coding: utf-8 -*-
 
-
-#Exemple command
-#
-# python3 API_KEY.py 127.0.0.1:80 create
-# python3 API_KEY.py 127.0.0.1:80 list B4B018AA61
-# python3 API_KEY.py 127.0.0.1:80 delete A2615C25A6 CA606DA036
-
 import sys
 import urllib,json
 from urllib import request, parse
 
-ip = sys.argv[1]
-action = sys.argv[2]
-data = sys.argv[3:]
-
+try:
+    ip = sys.argv[1]
+    action = sys.argv[2]
+    data = sys.argv[3:]
+except:
+    action = 'error'
 
 if action == 'create':
     query_args = '{"devicetype":"Domoticz"}'
@@ -27,14 +22,14 @@ if action == 'create':
         response = response.decode("utf-8", "ignore")
         j = eval(response)
         print ("Your new API key is : " + j[0]["success"]['username'])
-        
+
     except urllib.error.URLError as e:
         if e.code == 403:
             print('Please unlock the gateway first and retry !')
-            
+
 elif action == 'list':
     if len(data) < 1:
-        print('Missing params!')
+        print('Missing params !')
     else:
         req = request.Request('http://' + ip + '/api/' + data[0] + '/config')
         response = request.urlopen(req).read()
@@ -46,13 +41,34 @@ elif action == 'list':
 
 elif action == 'delete':
     if len(data) < 2:
-        print('Missing params!')
+        print('Missing params !')
     else:
         req = request.Request('http://' + ip + '/api/' + data[0] + '/config/whitelist/' + data[1] , method='DELETE')
         try:
             response = request.urlopen(req).read()
             print ('Key deleted')
-            
+
         except urllib.error.URLError as e:
             print('Error : ' + str(e.code))
             print(e.read())
+
+elif action == 'info':
+    if len(data) < 1:
+        print('Missing params!')
+    else:
+        req = request.Request('http://' + ip + '/api/' + data[0] + '/config')
+        response = request.urlopen(req).read()
+        response = response.decode("utf-8", "ignore")
+        j = json.loads(response)
+        print ("Webscoket Port : " + str(j['websocketport']) )
+        print ("IP adress: " + j['ipaddress'] )
+        print ("Firmware version : " + j['fwversion'] )
+
+
+else:
+    print('Exemples of use')
+    print('python3 API_KEY.py <ip>:<port> <command> <Api key> <data>\n')
+    print('python3 API_KEY.py 127.0.0.1:80 create')
+    print('python3 API_KEY.py 127.0.0.1:80 info B4B018AA61')
+    print('python3 API_KEY.py 127.0.0.1:80 list B4B018AA61')
+    print('python3 API_KEY.py 127.0.0.1:80 delete B4B018AA61 CA606DA036')
