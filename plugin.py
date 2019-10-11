ff02f31cb2a41449350b666a5be8e3ccadb09564
Smanar <aurelien.pra@laposte.net>
2019-10-11 19:15:15 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1225,7 +1225,7 @@ def CreateDevice(IEEE,_Name,_Type):
         kwarg['Type'] = 244
         kwarg['Subtype'] = 62
         kwarg['Switchtype'] = 18
-        kwarg['Options'] = {"LevelActions": "|||", "LevelNames": "Off|Heat|Auto", "LevelOffHidden": "true", "SelectorStyle": "0"}
+        kwarg['Options'] = {"LevelActions": "|||", "LevelNames": "Off|Heat|Auto", "LevelOffHidden": "false", "SelectorStyle": "0"}
 
     #groups
     elif _Type == 'LightGroup' or _Type == 'groups':
