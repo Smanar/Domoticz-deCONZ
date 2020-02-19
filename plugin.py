1e928528b0efc59e8fafbd9d5dabc71cab19a1fa
Smanar <aurelien.pra@laposte.net>
2020-02-19 18:23:43 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1290,7 +1290,7 @@ def CreateDevice(IEEE,_Name,_Type):
         kwarg['Subtype'] = 62
         kwarg['Switchtype'] = 18
         kwarg['Image'] = 9
-        kwarg['Options'] = {"LevelActions": "||||||||", "LevelNames": "Off|Shak|Wake|Drop|90ֲ°|180ֲ°|Push|Tap", "LevelOffHidden": "true", "SelectorStyle": "0"}
+        kwarg['Options'] = {"LevelActions": "||||||||", "LevelNames": "Off|Shak|Wake|Drop|90°|180°|Push|Tap", "LevelOffHidden": "true", "SelectorStyle": "0"}
 
     elif _Type == 'XCube_R':
         kwarg['TypeName'] = 'Custom'
