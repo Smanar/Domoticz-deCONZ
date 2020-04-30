c74266d473ae13fd035f4c4d813a06eb0ce20f87
Smanar <aurelien.pra@laposte.net>
2020-04-30 18:11:55 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1220,12 +1220,12 @@ def CreateDevice(IEEE,_Name,_Type):
         kwarg['TypeName'] = 'Illumination'
 
     elif _Type == 'ZHAConsumption':# in kWh
-        #Device with only comsumption, not exist (yet)
+        #Device with only comsumption
         if True: # if IEEE.endswith('0702'):
             kwarg['Type'] = 113
             kwarg['Subtype'] = 0
             kwarg['Switchtype'] = 0
-        #Device with power and energy
+        #Device with power and energy, not exist (yet)
         else:
             kwarg['TypeName'] = 'kWh'
 
