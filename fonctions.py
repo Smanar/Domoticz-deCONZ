470036a746621d11a763f40307d650bdb08ad3f4
Smanar <aurelien.pra@laposte.net>
2020-05-02 14:53:28 +0200

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -227,6 +227,40 @@ def JSON_Repair(data):
 
     return '[' + b + ']'
 
+
+
+#*********************************
+# Table for switch
+#
+#*********************************
+
+XiaomiDoubleGangButtonSwitchTable = ['1002','2002','3002','1004','2004','3004','1001','2001','3001']
+
+#eyal start
+#10 - B1 - 1000
+#20 - B2 - 2000
+#30 - B3 - 3000
+#40 - B4 - 4000
+#50 - B5 - 5000
+#60 - B6 - 6000
+#70 - B1L - Not working yet
+#80 - B2L - Not working yet
+#90 - B3L - 3001 press, 3003 release
+#100 - B4L - 4001 press, 4003 release
+#110 - B5L - 5001 press, 5003 release
+#120 - B6L - 6001 press, 5003 release (cannot be distinguished from B5L)
+#130 - B1D - Not working yet
+#140 - B2D - Not working yet
+#150 - B3D - Not working yet
+#160 - B4D - Not working yet
+#170 - B5D - Not working yet
+#180 - B6D - Not working yet
+XiaomiOpple6ButtonSwitchTable = ['1000','2000','3000','4000','5000','6000','9999','9999','3003','4003','5003']
+#eyal end
+
+
+
+
 #**************************************************************************************************
 # Domoticz fonctions
 #
@@ -605,58 +639,6 @@ def ButtonconvertionTradfriRemote(val):
 
     return kwarg
 
-#eyal start
-#10 - B1 - 1000
-#20 - B2 - 2000
-#30 - B3 - 3000
-#40 - B4 - 4000
-#50 - B5 - 5000
-#60 - B6 - 6000
-#70 - B1L - Not working yet
-#80 - B2L - Not working yet
-#90 - B3L - 3001 press, 3003 release
-#100 - B4L - 4001 press, 4003 release
-#110 - B5L - 5001 press, 5003 release
-#120 - B6L - 6001 press, 5003 release (cannot be distinguished from B5L)
-#130 - B1D - Not working yet
-#140 - B2D - Not working yet
-#150 - B3D - Not working yet
-#160 - B4D - Not working yet
-#170 - B5D - Not working yet
-#180 - B6D - Not working yet
-
-def ButtonconvertionXiaomiOpple6ButtonSwitch(val):
-    kwarg = {}
-    val = str(val)
-
-    if val == '1000' or val == 1000:
-        kwarg['nValue'] = 10
-    elif val == '2000' or val == 2000:
-        kwarg['nValue'] = 20
-    elif val == '3000' or val == 3000:
-        kwarg['nValue'] = 30
-    elif val == '4000' or val == 4000:
-        kwarg['nValue'] = 40
-    elif val == '5000' or val == 5000:
-        kwarg['nValue'] = 50
-    elif val == '6000' or val == 6000:
-        kwarg['nValue'] = 60
-    elif val == '3003' or val == 3003:
-        kwarg['nValue'] = 90
-    elif val == '4003' or val == 4003:
-        kwarg['nValue'] = 100
-    elif val == '5003' or val == 5003:
-        kwarg['nValue'] = 110
-    else:
-        kwarg['nValue'] = 0
-
-    if kwarg['nValue'] == 0:
-        kwarg['sValue'] = 'Off'
-    else:
-        kwarg['sValue'] = str( kwarg['nValue'] )
-
-    return kwarg
-#eyal end
 def ButtonconvertionTradfriSwitch(val):
     kwarg = {}
     val = str(val)
@@ -685,28 +667,47 @@ def ButtonconvertionTradfriSwitch(val):
 
     return kwarg
 
-def ButtonconvertionGeneric(val):
+def ButtonConvertion(val,model = 0):
     kwarg = {}
-    val = "%04d" % val
-    Button_Number = val[0]
-    Button_Action = val[3]
 
-    v = 0
+    kwarg['nValue'] = 0
 
-    if Button_Action == '2': #  Release (after press)
-        v = 10
-    if Button_Action == '3': # Release (after hold)
-        v = 20
-    if Button_Action == '4': # Double press
-        v = 30
-    if Button_Action == '5': # Triple press
-        v = 40
-    if Button_Action == '6': # Quadruple press
-        v = 50
-    if Button_Action == '7': # shake
-        v = 60
+    #Generic procedure
+    if model == 0:
 
-    kwarg['nValue'] = v * int(Button_Number)
+        val = "%04d" % val
+        Button_Number = val[0]
+        Button_Action = val[3]
+
+        v = 0
+
+        if Button_Action == '2': #  Release (after press)
+            v = 10
+        if Button_Action == '3': # Release (after hold)
+            v = 20
+        if Button_Action == '4': # Double press
+            v = 30
+        if Button_Action == '5': # Triple press
+            v = 40
+        if Button_Action == '6': # Quadruple press
+            v = 50
+        if Button_Action == '7': # shake
+            v = 60
+
+        kwarg['nValue'] = v * int(Button_Number)
+
+    else:
+        val = str(val)
+
+        #Xaomi double gang
+        if model == 1:
+            if val in XiaomiDoubleGangButtonSwitchTable:
+                kwarg['nValue'] = 10 * (1 + XiaomiDoubleGangButtonSwitchTable.index(val))
+        #Xaomi Opple 6 buttons
+        if model == 2:
+            if val in XiaomiOpple6ButtonSwitchTable:
+                kwarg['nValue'] = 10 * (1 + XiaomiOpple6ButtonSwitchTable.index(val))
+
 
     if kwarg['nValue'] == 0:
         kwarg['sValue'] = 'Off'
