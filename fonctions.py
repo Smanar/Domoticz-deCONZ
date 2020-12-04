b0ffeb627be0b1611ecb3b25962187a99e95f57c
Smanar <aurelien.pra@laposte.net>
2020-12-04 19:40:18 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -173,6 +173,51 @@ def xy_to_rgb(x, y, brightness = 1):
     return {'r': int(r * 255), 'g': int(g * 255), 'b': int(b * 255)}
 
 
+def rgb_to_hsv(rgb):
+    ''' convert rgb tuple to hls tuple '''
+    r, g, b = rgb
+    r = float(r/255)
+    g = float(g/255)
+    b = float(b/255)
+    high = max(r, g, b)
+    low = min(r, g, b)
+    h, s, v = high, high, high
+
+    d = high - low
+    s = 0 if high == 0 else d/high
+
+    if high == low:
+        h = 0.0
+    else:
+        h = {
+            r: (g - b) / d + (6 if g < b else 0),
+            g: (b - r) / d + 2,
+            b: (r - g) / d + 4,
+        }[high]
+        h /= 6
+
+    return h, s, v
+
+
+def hsv_to_rgb(h, s, v):
+    i = math.floor(h*6)
+    f = h*6 - i
+    p = v * (1-s)
+    q = v * (1-f*s)
+    t = v * (1-(1-f)*s)
+
+    r, g, b = [
+        (v, t, p),
+        (q, v, p),
+        (p, v, t),
+        (p, q, v),
+        (t, p, v),
+        (v, p, q),
+    ][int(i%6)]
+
+    return r, g, b
+
+
 def Count_Type(d):
     b = l = s = g = o = c = 0
     for i in d:
