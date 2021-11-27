71e5f75f0f847f9c6aa9571419e83fdceb81969f
Smanar <aurelien.pra@laposte.net>
2021-11-27 09:32:36 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -619,8 +619,8 @@ class BasePlugin:
                         return
                 elif 'TRADFRI remote control' in Model:
                     Type = 'Tradfri_remote'
-                #elif 'RWL021' in Model:
-                #    Type = 'Tradfri_remote'
+                elif 'Remote Control N2' in Model:
+                    Type = 'Tradfri_remote'
                 elif 'TRADFRI on/off switch' in Model:
                     Type = 'Tradfri_on/off_switch'
                 elif 'lumi.remote.b186acn01' in Model:
