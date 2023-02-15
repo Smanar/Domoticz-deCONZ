f83d1dbb7385bdf5a99040eee4359826d6129077
Smanar <aurelien.pra@laposte.net>
2023-02-15 18:04:58 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -13,9 +13,9 @@
         <ul style="list-style-type:square">
             <li>You can use the file API_KEY.py if you have problems to get your API Key or your Websocket Port.</li>
             <li>You can find updated files for deCONZ on their github : https://github.com/dresden-elektronik/deconz-rest-plugin.</li>
-            <li>If you want the plugin works without connexion, use as IP 127.0.0.1 (if deCONZ and domoticz are on same machine).</li>
+            <li>If you want the plugin works without connection, use as IP 127.0.0.1 (if deCONZ and domoticz are on same machine).</li>
             <li>If you are running the plugin for the first time, better to enable debug log (Take Debug info Only).</li>
-            <li>You can find a front-end on the Domoticz menu "Custom" > "Deconz", you can use it to get the API key or configure sensors.</li>
+            <li>You can find a front-end on the Domoticz menu "Custom" > "Deconz", you can use it to get the API key, configure sensors or alarm system.</li>
         </ul>
         <h3>Supported Devices</h3>
         <ul style="list-style-type:square">
@@ -263,7 +263,7 @@ class BasePlugin:
                     url = '/api/' + Parameters["Mode2"] + '/alarmsystems/1/' + ['disarm','arm_away','arm_stay','arm_night'][int(Level/10)]
                     self.SendCommand(url,{'code0':str(Devices[Unit].Description)})
                 else:
-                    Domoticz.Error("Missing code0 in alamr system widget description")
+                    Domoticz.Error("Missing code0 in alarm system widget description")
             else:
                 Domoticz.Error("Device not ready : " + str(Unit) )
             return
@@ -1033,10 +1033,6 @@ class BasePlugin:
         elif 'attr' in _Data:
             attr = _Data['attr']
 
-        #MAJ capabilities, not used yet
-        elif 'capabilities' in _Data:
-            capabilities = _Data['capabilities']
-
         else:
             Domoticz.Error("Unknow MAJ: " + str(_Data) )
 
@@ -1370,7 +1366,7 @@ def UpdateDeviceProc(kwarg,Unit):
     #if (kwarg['nValue'] == Devices[Unit].nValue) and (kwarg['nValue'] == Devices[Unit].nValue) and ('Color' not in kwarg):
     #    kwarg['SuppressTriggers'] = True
 
-    #Alaways update for Color Bulb
+    #Always update for Color Bulb
     if 'Color' in kwarg:
         NeedUpdate = True
 
@@ -1393,8 +1389,18 @@ def UpdateDeviceProc(kwarg,Unit):
     if 'sValue' not in kwarg:
         kwarg['sValue'] = Devices[Unit].sValue
 
+    #Do we need to update the special battery sensor ?
+    if ENABLEBATTERYWIDGET:
+        if 'BatteryLevel' in kwarg and kwarg['BatteryLevel'] != 255:
+            NewIEE = Devices[Unit].DeviceID.split("-")[0]
+            Unit2 = GetDomoDeviceInfo(NewIEE + '_battery')
+            if Unit2 and getattr(Devices[Unit2],'BatteryLevel') != kwarg['BatteryLevel']:
+                kwarg2 = {"nValue":0, "sValue":str(kwarg["BatteryLevel"])}
+                Devices[Unit2].Update(**kwarg2)
+                Domoticz.Debug("### Update special device ("+NewIEE+") : " + str(kwarg))
+
     if NeedUpdate or not LIGHTLOG:
-        Domoticz.Debug("### Update  device ("+Devices[Unit].Name+") : " + str(kwarg))
+        Domoticz.Debug("### Update device ("+Devices[Unit].Name+") : " + str(kwarg))
 
         #Disable offline light ?
         if (Devices[Unit].Type == 241) or ((Devices[Unit].Type == 244) and (Devices[Unit].SubType == 73) and (Devices[Unit].SwitchType == 7)):
@@ -1404,16 +1410,6 @@ def UpdateDeviceProc(kwarg,Unit):
                kwarg['sValue'] = 'Off'
 
         Devices[Unit].Update(**kwarg)
-
-        #Do we need to update the special battery sensor ?
-        if ENABLEBATTERYWIDGET:
-            if 'BatteryLevel' in kwarg and kwarg['BatteryLevel'] != 255:
-                NewIEE = Devices[Unit].DeviceID.split("-")[0]
-                Unit2 = GetDomoDeviceInfo(NewIEE + '_battery')
-                if Unit2:
-                    kwarg2 = {"nValue":0, "sValue":str(kwarg["BatteryLevel"])}
-                    Devices[Unit2].Update(**kwarg2)
-                    Domoticz.Debug("### Update special device ("+NewIEE+") : " + str(kwarg))
 
     else:
         Domoticz.Debug("### Update device ("+Devices[Unit].Name+") : " + str(kwarg) + ", IGNORED , no changes !")
