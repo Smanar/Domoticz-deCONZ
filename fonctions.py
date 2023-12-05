80526a0cd92c6279a9836310a843f1090c88784f
Smanar <aurelien.pra@laposte.net>
2023-12-05 17:20:13 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -4,8 +4,8 @@
 from struct import unpack
 import json
 
-import DomoticzEx as Domoticz
-
+#import DomoticzEx as Domoticz
+import Domoticz
 buffercommand = {}
 
 BOOLEAN_SENSOR = ['flag', 'water', 'fire', 'presence', 'carbonmonoxide', 'daylight', 'alarm', 'lock']
